cef3c10695be8e9db97f7c77fa6f1ad661d2387e
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-06-09 23:01:13 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::env;
+
 fn main() {
+	let arguments: Vec<String> = env::args()
+		.skip(1)
+		.collect(); 
+	if arguments.is_empty() { /* enter repl */ return };
 	// SPEC:
 	//	Adding & Removing
 	//	Finishing (maybe also unfinishing(via undo command?)) (the same as Removing?)
